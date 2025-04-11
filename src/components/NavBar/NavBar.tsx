eb307b00fa54cb9c691b539a1f2d444bdb98eef1
Ayoola Olumide Olufemi <ayoolaolumide98@gmail.com>
2025-04-11 08:46:16 +0100

    fix in navbar position style

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -5,7 +5,7 @@ import MobileNav from './MobileNav'
 
 const NavBar = () => {
   return (
-    <nav className='bg-(--white) flex items-center justify-between px-10 md:px-20 sticky top-0 z-10 shadow-2xl relative'>
+    <nav className='bg-(--white) flex items-center justify-between px-10 md:px-20 sticky top-0 z-10 shadow-2xl'>
         <div className='w-[168px] h-[100px]'>
             <Image src="/logo.svg" alt="logo" width={168} height={100}/>
         </div>
